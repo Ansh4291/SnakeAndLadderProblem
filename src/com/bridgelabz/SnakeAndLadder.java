52ed53a1c6bbcfb0f83102c890de8fb9a910e5fa
AnshulRathore <anshulrathore1995@gmail.com>
2022-04-25 23:41:47 +0530

    Added: Player Rolls A dice with Random Num.

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import javax.swing.text.Position;
+import java.util.Random;
 
 public class SnakeAndLadder {
     public static void main(String[] args) {
@@ -9,5 +10,10 @@ public class SnakeAndLadder {
         int SingPlyrPos = 0;
         System.out.println("Player Position is:- 0 ");
         System.out.println("So Lets Start the game play ");
+
+        Random random = new Random();
+        System.out.println("The Player is rolling a die With a good Expetation :-) ");
+        int die = random.nextInt(6) + 1;
+        System.out.println("Dice Will get Random Number is :- " + die);
     }
 }
