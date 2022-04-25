7d4d654520386c4c8d89da41974f041919c454a5
AnshulRathore <anshulrathore1995@gmail.com>
2022-04-25 23:14:14 +0530

    Added: Staring Position of Player is Zero Added

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,13 @@
 package com.bridgelabz;
 
+import javax.swing.text.Position;
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to the Snake And Ladder Game :-) ");
+        //Starting Postion of player is Start with 0
+        int SingPlyrPos = 0;
+        System.out.println("Player Position is:- 0 ");
+        System.out.println("So Lets Start the game play ");
     }
 }
